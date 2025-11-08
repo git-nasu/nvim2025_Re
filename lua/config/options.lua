cc2dadfab0b840031ea1b55bed11802c717461f2
itsuki00 <nasu301130@gmail.com>
2025-11-08 15:48:48 +0900

    Added to options.lua for ssh clipboard(NixOS on UTM) settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,20 @@ vim.g.lazyvim_picker = "snacks"
 
 vim.opt.virtualedit = "onemore"
 vim.opt.timeoutlen = 300
+
+-- ssh clipboard settings for NixOS on UTM
+if vim.fn.has("macunix") == 1 and vim.env.SSH_CONNECTION ~= nil then
+  vim.opt.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "macOS-clipboard",
+    copy = {
+      ["+"] = "pbcopy",
+      ["*"] = "pbcopy",
+    },
+    paste = {
+      ["+"] = "pbpaste",
+      ["*"] = "pbpaste",
+    },
+    cache_enabled = 0,
+  }
+end
